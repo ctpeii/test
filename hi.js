8320089ac3af7718235931d2d41a4b6915d42e04
ctpeii <romankatulkin@gmail.com>
2021-07-19 14:49:15 +0300

    test

---
+++ hi.js
@@ -0,0 +1,20 @@
+// setTimeout(reminder, 5000);
+// function reminder() {
+//     alert( 'Hi There' );
+// } 
+// infinity 
+// 1 / 0
+// console.log();
+let tf = prompt('при какой температуре по фаренгейту горит книга Бредбери?')
+let tc = (tf - 32) * 5/9
+if (tf == 456) {
+    alert(`Бредбери ошибся, и при ${Math.round(tc)} градусах по цельсию, книга просто очень горячая, хотя она и так в целом неплоха`)
+}
+else {
+    alert(`в цельсиях это ${Math.round(tc)} но книгу ты не читал`)
+}
+
+
+
+
+
